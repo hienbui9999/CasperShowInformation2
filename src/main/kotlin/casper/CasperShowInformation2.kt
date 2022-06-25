7cf16e662c48db4dec9bc708be998ae405d3f76b
hienbui9999 <buiduyhien@gmail.com>
2022-06-25 17:46:21 +0700

    init project

---
+++ src/main/kotlin/casper/CasperShowInformation2.kt
@@ -0,0 +1,10 @@
+package casper
+
+class CasperShowInformation2 {
+    public fun SampleCall() {
+        println("Sample call from Casper Show Information")
+    }
+    public fun SampleCall2(a:String) {
+        println("Sample call 2 from Casper show INformation with a: " + a)
+    }
+}
